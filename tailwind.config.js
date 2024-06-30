14363ddb30f66d9d6103c65849b586f46ea58b2c
shariful-alam-2024 <outsourcingshorif2024@gmail.com>
2024-06-30 09:42:52 +0600

    first-project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'Head':'#1C1E53',
+        'Button':'#FCD980',
+        'BGround':'#1C1E53',
+        
+        'Arr':'#2405F2',
+        'Foot':'#1C1E53',
+        'Illus':'#F4F6FC',
+        'Aia':'#FCD980',
+      }
+    },
+  },
+  plugins: [],
+}
+
